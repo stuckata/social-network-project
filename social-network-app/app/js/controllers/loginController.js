ba2118dab78296393f8b42190a57b270d459ec75
stuckata <stu.kirkov@gmail.com>
2015-05-20 14:47:38 +0300

    noty library added

--- social-network-app/app/js/controllers/loginController.js
+++ social-network-app/app/js/controllers/loginController.js
@@ -1,12 +1,16 @@
 "use strict";
 
-app.controller('LoginController', ['$scope', '$location', '$timeout', 'userData', function ($scope, $location, $timeout, userData) {
-	
+app.controller('LoginController', ['$scope', '$location', '$timeout', 'userData', 'notyModule', 'notification', 
+	function ($scope, $location, $timeout, userData, notyModule, notification) {
+
 	$scope.login = function (user) {
 		userData.login(user)
 			.$promise
 			.then(function (data) {
-				$location.path('/home');
-			});	
+			$scope.showNotification = function () {
+				noty.show('Login successful',"success")
+			};
+			$location.path('/home');
+		});
 	};
 }]);
