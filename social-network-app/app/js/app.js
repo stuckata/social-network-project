e6862aa29eac1685acded1bdd478f3816ffc5ce8
stuckata <stu.kirkov@gmail.com>
2015-05-29 17:38:41 +0300

    infinite scroll added for user homepage feed

--- social-network-app/app/js/app.js
+++ social-network-app/app/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module('SocialNetworkApp', ['ngRoute', 'ngResource', 'LocalStorageModule', 'ui.bootstrap']);
+var app = angular.module('SocialNetworkApp', ['ngRoute', 'ngResource', 'LocalStorageModule', 'ui.bootstrap', 'infinite-scroll']);
 
 app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api/');
 
--- social-network-app/app/js/controllers/HomeController.js
+++ social-network-app/app/js/controllers/HomeController.js
@@ -2,292 +2,311 @@
 
 app.controller('HomeController', function ($modal, $scope, $rootScope, $location, $routeParams, authenticationService, notificationService, userService, postsService, profileService, commentsService) {
 
-	if (!authenticationService.isLoggedIn()) {
-		$location.path("/");
-		notificationService.showInfo("You must login to proceed!");
-	}
+    if (!authenticationService.isLoggedIn()) {
+        $location.path("/");
+        notificationService.showInfo("You must login to proceed!");
+    }
 
-	userService.fetchMyFriends(
-		function success(data) {
-			$scope.friends = data;
-		},
-		function error(error) {
-			notificationService.showError("Problem while fetching current user friends", error);
-			console.log(error);
-		}
-		);
+    userService.fetchMyFriends(
+        function success(data) {
+            $scope.friends = data;
+        },
+        function error(error) {
+            notificationService.showError("Problem while fetching current user friends", error);
+            console.log(error);
+        }
+    );
 
-	userService.fetchMyWall("", 10,
-		function success(data) {
-			$scope.wall = data;
-		},
-		function error(error) {
-			notificationService.showError("Problem while fetching current user wall posts", error);
-			console.log(error);
-		}
-		);
+    userService.fetchMyWall("", 10,
+        function success(data) {
+            $scope.wall = data;
+        },
+        function error(error) {
+            notificationService.showError("Problem while fetching current user wall posts", error);
+            console.log(error);
+        }
+    );
 
+    $scope.loadMore = function() {
+        var postId = $scope.wall[$scope.wall.length - 1].id;
+        userService.fetchMyWall(postId, 10,
+            function success(data) {
+                console.log(data);
+                if(data != []) {
+                    for(var i = 0; i < data.length; i++) {
+                        $scope.wall.push(data[i]);
 
-	$scope.openUserProfile = function (selectedUser) {
-		userService.preview(selectedUser, function success(data) {
-			console.log(data);
-			$scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
-			sessionStorage['selectedUser'] = JSON.stringify(data);
-			if ($scope.currentUserInfo.id == data.id) {
-				$location.path("/home");
-			} else {
-				$location.path("/user-profile");
-			}
+                    }
+                }
+            },
+            function error(error) {
+                notificationService.showError("Problem while fetching current user wall posts", error);
+                console.log(error);
+            }
+        );
 
-		},
-			function error(error) {
-				notificationService.showError("Navigation to selected profile failed", error);
-				console.log(error);
-			});
-	};
+    };
 
-	userService.getMyInfo(
-		function success(data) {
-			$scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
-		},
-		function error(error) {
-			notificationService.showError("Problem while fetching current user data", error);
-			console.log(error);
-		}
-		);
+    $scope.openUserProfile = function (selectedUser) {
+        userService.preview(selectedUser, function success(data) {
+                console.log(data);
+                $scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
+                sessionStorage['selectedUser'] = JSON.stringify(data);
+                if ($scope.currentUserInfo.id == data.id) {
+                    $location.path("/home");
+                } else {
+                    $location.path("/user-profile");
+                }
 
-	$scope.search = function (searchTerm) {
-		userService.search(searchTerm, function success(data) {
-			$scope.searchResults = data;
-		},
-			function error(error) {
-				notificationService.showError("Problem while fetching searched data", error);
-				console.log(error);
-			});
-	};
+            },
+            function error(error) {
+                notificationService.showError("Navigation to selected profile failed", error);
+                console.log(error);
+            });
+    };
 
-	$scope.getFriends = function () {
-		$location.path("/friends");
-	};
+    userService.getMyInfo(
+        function success(data) {
+            $scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
+        },
+        function error(error) {
+            notificationService.showError("Problem while fetching current user data", error);
+            console.log(error);
+        }
+    );
 
-	profileService.fetchRequests(
-		function success(data) {
-			$scope.requests = data;
-		},
-		function error(error) {
-			notificationService.showError("Problem while fetching friend requests", error);
-			console.log(error);
-		}
-		);
+    $scope.search = function (searchTerm) {
+        userService.search(searchTerm, function success(data) {
+                $scope.searchResults = data;
+            },
+            function error(error) {
+                notificationService.showError("Problem while fetching searched data", error);
+                console.log(error);
+            });
+    };
 
-	var comment = {};
-	$scope.postComment = function (post, commentContent, message) {
-		comment.CommentContent = commentContent;
-		commentsService.postCommnent(post.id, comment, function success(data) {
-			post.comments.unshift(data);
-		},
-			function error(error) {
-				notificationService.showError("Problem while posting the message", error);
-				console.log(error);
-			});
-	};
+    $scope.getFriends = function () {
+        $location.path("/friends");
+    };
 
-	$scope.deletePost = function (postId) {
-		postsService.deletePost(postId,
-			function success(data) {
-			},
-			function error(error) {
-				notificationService.showError("Problem to delete this post", error);
-				console.log(error);
-			}
-		);
-	};
+    profileService.fetchRequests(
+        function success(data) {
+            $scope.requests = data;
+        },
+        function error(error) {
+            notificationService.showError("Problem while fetching friend requests", error);
+            console.log(error);
+        }
+    );
 
-	$scope.likePost = function (postId) {
-		postsService.publishPostLikes(postId,
-			function success(data) {
-			},
-			function error(error) {
-				notificationService.showError("Problem liking this...", error);
-				console.log(error);
-			}
-		);
-	};
+    var comment = {};
+    $scope.postComment = function (post, commentContent, message) {
+        comment.CommentContent = commentContent;
+        commentsService.postCommnent(post.id, comment, function success(data) {
+                post.comments.unshift(data);
+            },
+            function error(error) {
+                notificationService.showError("Problem while posting the message", error);
+                console.log(error);
+            });
+    };
 
-	$scope.deletePostLike = function (postId) {
-		postsService.deletePostLikes(postId,
-			function success(data) {
-			},
-			function error(error) {
-				notificationService.showError("Problem to delete this like", error);
-				console.log(error);
-			}
-		);
-	};
+    $scope.deletePost = function (postId) {
+        postsService.deletePost(postId,
+            function success(data) {
+            },
+            function error(error) {
+                notificationService.showError("Problem to delete this post", error);
+                console.log(error);
+            }
+        );
+    };
 
-	$scope.deleteComment = function (postId, commentId) {
-		postsService.deleteComment(postId, commentId,
-			function success(data) {
-			},
-			function error(error) {
-				notificationService.showError("Problem to delete this comment", error);
-				console.log(error);
-			}
-		);
-	};
+    $scope.likePost = function (postId) {
+        postsService.publishPostLikes(postId,
+            function success(data) {
+            },
+            function error(error) {
+                notificationService.showError("Problem liking this...", error);
+                console.log(error);
+            }
+        );
+    };
 
-	$scope.animationsEnabled = false;
-	$scope.openDialog = function (size, templateUrl) {
-		$scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
-		$scope.username = $scope.currentUserInfo.username;
-		var modalInstance = $modal.open({
-			animation: $scope.animationsEnabled,
-			templateUrl: templateUrl,
-			controller: 'ModalInstanceCtrl',
-			size: size,
-			resolve: {
-				username: function () {
-					return $scope.username;
-				},
-				wall: function () {
-					return $scope.wall;
-				},
-				requests: function () {
-					return $scope.requests;
-				},
-				friends: function () {
-					return $scope.friends;
-				},
-				user: function () {
-					return $scope.currentUserInfo;
-				}
-			}
-		});
+    $scope.deletePostLike = function (postId) {
+        postsService.deletePostLikes(postId,
+            function success(data) {
+            },
+            function error(error) {
+                notificationService.showError("Problem to delete this like", error);
+                console.log(error);
+            }
+        );
+    };
 
-		modalInstance.result.then(function () {
-		}, function () {
-				console.log('Modal dismissed at: ' + new Date());
-			});
-	};
+    $scope.deleteComment = function (postId, commentId) {
+        postsService.deleteComment(postId, commentId,
+            function success(data) {
+            },
+            function error(error) {
+                notificationService.showError("Problem to delete this comment", error);
+                console.log(error);
+            }
+        );
+    };
+
+    $scope.animationsEnabled = false;
+    $scope.openDialog = function (size, templateUrl) {
+        $scope.currentUserInfo = JSON.parse(sessionStorage['currentUserInfo']);
+        $scope.username = $scope.currentUserInfo.username;
+        var modalInstance = $modal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: templateUrl,
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                username: function () {
+                    return $scope.username;
+                },
+                wall: function () {
+                    return $scope.wall;
+                },
+                requests: function () {
+                    return $scope.requests;
+                },
+                friends: function () {
+                    return $scope.friends;
+                },
+                user: function () {
+                    return $scope.currentUserInfo;
+                }
+            }
+        });
+
+        modalInstance.result.then(function () {
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
 
 
 });
 
 app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, postsService, notificationService, profileService, username, wall, requests, friends, user) {
 
-	$scope.username = username;
-	$scope.wall = wall;
-	$scope.requests = requests;
-	$scope.friends = friends;
-	$scope.user = user;
+    $scope.username = username;
+    $scope.wall = wall;
+    $scope.requests = requests;
+    $scope.friends = friends;
+    $scope.user = user;
 
-	var post = {};
-	$scope.postMessage = function (message) {
+    var post = {};
+    $scope.postMessage = function (message) {
 
-		post.username = $scope.username;
-		post.postContent = message;
+        post.username = $scope.username;
+        post.postContent = message;
 
-		postsService.publishPost(post, function success(data) {
-			$scope.wall.unshift(data);
-			$modalInstance.dismiss('cancel');
-		},
-			function error(error) {
-				notificationService.showError("Problem while posting the message", error);
-				console.log(error);
-			});
-	};
+        postsService.publishPost(post, function success(data) {
+                $scope.wall.unshift(data);
+                $modalInstance.dismiss('cancel');
+            },
+            function error(error) {
+                notificationService.showError("Problem while posting the message", error);
+                console.log(error);
+            });
+    };
 
-	$scope.approveRequest = function (requestId, index) {
-		profileService.acceptRequest(requestId,
-			function success(data) {
-				$scope.friends.unshift(data);
-				$scope.requests.splice(index, 1);
-				notificationService.showInfo("Congrats! You have a new friend!");
-			},
-			function error(error) {
-				notificationService.showError("Problem when approving request", error);
-				console.log(error);
-			}
-			);
-	};
+    $scope.approveRequest = function (requestId, index) {
+        profileService.acceptRequest(requestId,
+            function success(data) {
+                $scope.friends.unshift(data);
+                $scope.requests.splice(index, 1);
+                notificationService.showInfo("Congrats! You have a new friend!");
+            },
+            function error(error) {
+                notificationService.showError("Problem when approving request", error);
+                console.log(error);
+            }
+        );
+    };
 
-	$scope.rejectRequest = function (requestId, index) {
-		profileService.rejectRequest(requestId,
-			function success(data) {
-				$scope.friends.unshift(data);
-				$scope.requests.splice(index, 1);
-				notificationService.showInfo("You lost a friend successfuly!");
-			},
-			function error(error) {
-				notificationService.showError("Problem when rejecting request", error);
-				console.log(error);
-			}
-			);
-	};
+    $scope.rejectRequest = function (requestId, index) {
+        profileService.rejectRequest(requestId,
+            function success(data) {
+                $scope.friends.unshift(data);
+                $scope.requests.splice(index, 1);
+                notificationService.showInfo("You lost a friend successfuly!");
+            },
+            function error(error) {
+                notificationService.showError("Problem when rejecting request", error);
+                console.log(error);
+            }
+        );
+    };
 
-	$scope.updateUser = function (user) {
-		console.log(user);
-		profileService.updateUser(user,
-			function success(data) {
-				notificationService.showInfo("User data updated successfuly!");
-				$modalInstance.dismiss('cancel');
-			},
-			function error(error) {
-				notificationService.showError("Problem when updating user data", error);
-				console.log(error);
-			}
-			);
-	};
+    $scope.updateUser = function (user) {
+        console.log(user);
+        profileService.updateUser(user,
+            function success(data) {
+                notificationService.showInfo("User data updated successfuly!");
+                $modalInstance.dismiss('cancel');
+            },
+            function error(error) {
+                notificationService.showError("Problem when updating user data", error);
+                console.log(error);
+            }
+        );
+    };
 
-	$scope.coverImageSelected = function (fileInputField) {
-		var file = fileInputField.files[0];
-		console.log(file);
-		if (file.type.match(/image\/.*/)) {
-			var reader = new FileReader();
-			reader.onload = function () {
-				$scope.user.coverImageData = reader.result;
-				$(".image-box1").html("<img src='" + reader.result + "'>");
-			};
-			reader.readAsDataURL(file);
-		} else {
-			$(".image-box1").html("<p>File type not supported!</p>");
-		}
-	};
+    $scope.coverImageSelected = function (fileInputField) {
+        var file = fileInputField.files[0];
+        console.log(file);
+        if (file.type.match(/image\/.*/)) {
+            var reader = new FileReader();
+            reader.onload = function () {
+                $scope.user.coverImageData = reader.result;
+                $(".image-box1").html("<img src='" + reader.result + "'>");
+            };
+            reader.readAsDataURL(file);
+        } else {
+            $(".image-box1").html("<p>File type not supported!</p>");
+        }
+    };
 
-	$scope.profileImageSelected = function (fileInputField) {
-		var file = fileInputField.files[0];
-		console.log(file);
-		if (file.type.match(/image\/.*/)) {
-			var reader = new FileReader();
-			reader.onload = function () {
-				$scope.user.profileImageData = reader.result;
-				$(".image-box2").html("<img src='" + reader.result + "'>");
-			};
-			reader.readAsDataURL(file);
-		} else {
-			$(".image-box2").html("<p>File type not supported!</p>");
-		}
-	};
+    $scope.profileImageSelected = function (fileInputField) {
+        var file = fileInputField.files[0];
+        console.log(file);
+        if (file.type.match(/image\/.*/)) {
+            var reader = new FileReader();
+            reader.onload = function () {
+                $scope.user.profileImageData = reader.result;
+                $(".image-box2").html("<img src='" + reader.result + "'>");
+            };
+            reader.readAsDataURL(file);
+        } else {
+            $(".image-box2").html("<p>File type not supported!</p>");
+        }
+    };
 
-	$scope.passwordChange = function (userPass) {
-		profileService.changePassword(userPass,
-			function success(data) {
-				$modalInstance.dismiss('cancel');
-				notificationService.showInfo("User password updated successfuly!");
-			},
-			function error(error) {
-				notificationService.showError("Problem when updating user password", error);
-				console.log(error);
-			}
-			);
-	};
+    $scope.passwordChange = function (userPass) {
+        profileService.changePassword(userPass,
+            function success(data) {
+                $modalInstance.dismiss('cancel');
+                notificationService.showInfo("User password updated successfuly!");
+            },
+            function error(error) {
+                notificationService.showError("Problem when updating user password", error);
+                console.log(error);
+            }
+        );
+    };
 
 
-	$scope.ok = function () {
-		$modalInstance.close();
-	};
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
 
-	$scope.cancel = function () {
-		$modalInstance.dismiss('cancel');
-	};
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
 });
